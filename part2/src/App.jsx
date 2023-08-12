8314fca0c5f377cd9598e86a795072db7462f00f
tecnopulsar <tecnopulsar@gmail.com>
2023-08-12 20:09:37 -0300

    update conectando front y back

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
  //import InfoCourse_App from '../InfoCourse'
  // import Paises_App from '../Paises'
-import GuiaTelefonica from '../GuiaTelefonica'
-// import Notes from '../Notes'
+// import GuiaTelefonica from '../GuiaTelefonica'
+import Notes from '../Notes'
 
 
 function App() {
 
   return (
-    <GuiaTelefonica/>
+    // <GuiaTelefonica/>
     // <Paises_App/>
-    // <Notes/>
+    <Notes/>
   )
 }
 
